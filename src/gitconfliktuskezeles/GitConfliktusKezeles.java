ac2e900b4746168f4d57bc0b6af6c17a2b3751d8
SchmiedmeiszterAdam <smikuadam@gmail.com>
2022-03-09 11:48:18 +0100

    1.név inicializálása

--- src/gitconfliktuskezeles/GitConfliktusKezeles.java
+++ src/gitconfliktuskezeles/GitConfliktusKezeles.java
@@ -3,7 +3,7 @@ package gitconfliktuskezeles;
 public class GitConfliktusKezeles {
 
     public static void main(String[] args) {
-        String nev1;
+        String nev1 = "Ádám";
         String nev2;
     }
     
