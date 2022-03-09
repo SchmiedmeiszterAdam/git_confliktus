7b2e2bde21b612e822be509f03649bf967899eeb
SchmiedmeiszterAdam <78858027+SchmiedmeiszterAdam@users.noreply.github.com>
2022-03-09 11:53:56 +0100

    3. név deklarálása

--- src/gitconfliktuskezeles/GitConfliktusKezeles.java
+++ src/gitconfliktuskezeles/GitConfliktusKezeles.java
@@ -3,8 +3,10 @@ package gitconfliktuskezeles;
 public class GitConfliktusKezeles {
 
     public static void main(String[] args) {
+        int i = 7;
         String nev1 = "Ádám";
         String nev2 = "xy";
+        int nev3;
     }
     
 }
